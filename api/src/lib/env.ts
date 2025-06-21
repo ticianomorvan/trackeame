af9f704ebd98bd3b84943b79be202b58ee1c8509
Ticiano Morvan <ticiano.morvan@gmail.com>
2025-06-21 20:00:16 -0300

    update(api): make AWS-related environment variables optional as we are using instance metadata in production

--- api/src/lib/env.ts
+++ api/src/lib/env.ts
@@ -10,10 +10,9 @@ const envSchema = z.object({
   REDIS_PORT: z.coerce.number().default(6379),
 
   AWS_REGION: z.string().default("us-east-1"),
-  AWS_ACCESS_KEY_ID: z.string({ required_error: "AWS_ACCESS_KEY_ID is required"}),
-  AWS_SECRET_ACCESS_KEY: z.string({ required_error: "AWS_SECRET_ACCESS_KEY is required" }),
-  AWS_SES_SOURCE_ADDRESS: z.string({ required_error: "AWS_SES_SOURCE_ADDRESS is required"})
-    .email("AWS`_SES_SOURCE_ADDRESS must be a valid email address"),
+  AWS_ACCESS_KEY_ID: z.string().optional(),
+  AWS_SECRET_ACCESS_KEY: z.string().optional(),
+  AWS_SES_SOURCE_ADDRESS: z.string().email().optional(),
 
   FRONTEND_URL: z.string().url().default("http://localhost:5173"),
   DATABASE_URL: z.string().url().default("postgres://postgres:postgres@localhost:5432/trackeame"),
